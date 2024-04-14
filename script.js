10b339d67ad2a2867bbb6a5fa89236079a485970
Ashe <molly.02.andrews@gmail.com>
2024-04-14 17:51:38 -0400

    Added dropdown function to directory and styling

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDropdown(dropdownName) {
+    let dropdown = document.getElementById(dropdownName);
+    dropdown.style.display = (dropdown.style.display == 'block') ? 'none' : 'block';
+};
