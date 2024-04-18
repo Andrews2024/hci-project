566215c5f3fd585e39cb54efba35c5379fd06b43
Ashe <molly.02.andrews@gmail.com>
2024-04-18 00:24:23 -0400

    Added found item form and started toggling visibility

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleDropdown(dropdownName) {
     let dropdown = document.getElementById(dropdownName);
     dropdown.style.display = (dropdown.style.display == 'flex') ? 'none' : 'flex';
 };
+
+function openForm(formName) {
+    let form = document.getElementById(formName);
+    form.style.display = 'flex';
+};
