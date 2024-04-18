dde8562b9a12dc2cf451d9d3a1e3828432730057
Ashe <molly.02.andrews@gmail.com>
2024-04-18 14:47:51 -0400

    Added closing form and disappearing forum posts

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ function toggleDropdown(dropdownName) {
 };
 
 function openForm(formName) {
+    // Open the form
     let form = document.getElementById(formName);
     form.style.display = 'flex';
+
+    // Disable the forum post area
+    let forum = document.getElementById("post-container");
+    forum.style.display = 'none';
 };
+
+function closeForm(formName) {
+    // Close the form
+    let form = document.getElementById(formName);
+    form.style.display = 'none';
+
+    // Re-enable the forum post area
+    let forum = document.getElementById("post-container");
+    forum.style.display = 'flex';
+}
