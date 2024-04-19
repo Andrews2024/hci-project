89dbd1228d752d2aaca84f5c34b7486b2831ccff
Ashe <molly.02.andrews@gmail.com>
2024-04-18 22:29:37 -0400

    Added inserting user post to found item forum

--- script.js
+++ script.js
@@ -22,3 +22,55 @@ function closeForm(formName) {
     let forum = document.getElementById("post-container");
     forum.style.display = 'flex';
 }
+
+function postFoundItem() {
+    // Get inputs as strings
+    let itemText = document.getElementById("item-desc");
+    let foundLocation = document.getElementById("location-found");
+    let image = document.getElementById("found-img");
+    console.log(itemText.value, foundLocation.value, image.value);
+
+    // Get today's date in M/D/YYYY format
+    let date = new Date().toLocaleDateString();
+
+    // Strip "C:\fakepath\" from input image"
+    let imageFile = image.value.replace("C:\\fakepath\\", '');
+
+    // Make forum-post div and add to Found Items
+    // NOTE: NOT GOOD PRACTICE -- LEADS TO XSS ATTACKS
+    // Currently ignoring because this is a project to be locally hosted only
+    let newItem = `<div class="forum-item">
+                    <div class="forum-image">
+                        <img src="${imageFile}">
+                    </div>
+                    <div class="forum-labels">
+                        <label><b>Item</b>: ${itemText.value}</label><br>
+                        <label><b>Location</b>: ${foundLocation.value}</label><br>
+                        <label><b>Found On</b>: ${date}</label><br>
+                        <label><b>Email Me</b>: tester@wpi.edu</label>
+                    </div>
+                </div>`;
+    
+    let foundContainer = document.getElementById("found-header");
+    foundContainer.insertAdjacentHTML("afterend", newItem);
+
+    // Reset fields and close the form
+    itemText.value = '';
+    foundLocation.value = '';
+    image.value = '';
+    closeForm("found-form");
+}
+
+function resetAllForms() {
+    // Get all fields from found item form
+    let itemText = document.getElementById("item-desc");
+    let foundLocation = document.getElementById("location-found");
+    let image = document.getElementById("found-img");
+
+    // Get all fields from lost item form
+
+    // Reset fields
+    itemText.value = '';
+    foundLocation.value = '';
+    image.value = '';
+}
