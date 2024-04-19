9b64645f5f80044db34766607c0871bc68bed30a
Ashe <molly.02.andrews@gmail.com>
2024-04-18 22:46:03 -0400

    Added submitting form entry to Lost Items

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # TODO
-* Add forms for reporting lost and found items
-* Add adding entry to respective forum section when submitting a form
 * Add icons to buttons and labels
 * Add error messages to form entries
+* Add ability to remove divs with tester@wpi.edu email?
 
 # References
 Icons are from Flaticon: https://www.flaticon.com/uicons
--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function closeForm(formName) {
     // Re-enable the forum post area
     let forum = document.getElementById("post-container");
     forum.style.display = 'flex';
-}
+};
 
 function postFoundItem() {
     // Get inputs as strings
@@ -59,7 +59,45 @@ function postFoundItem() {
     foundLocation.value = '';
     image.value = '';
     closeForm("found-form");
-}
+};
+
+function postLostItem() {
+    // Get inputs as strings
+    let itemText = document.getElementById("item-desc-lost");
+    let foundLocation = document.getElementById("last-seen");
+    let image = document.getElementById("lost-img");
+    console.log(itemText.value, foundLocation.value, image.value);
+
+    // Get today's date in M/D/YYYY format
+    let date = new Date().toLocaleDateString();
+
+    // Strip "C:\fakepath\" from input image"
+    let imageFile = image.value.replace("C:\\fakepath\\", '');
+
+    // Make forum-post div and add to Lost Items
+    // NOTE: NOT GOOD PRACTICE -- LEADS TO XSS ATTACKS
+    // Currently ignoring because this is a project to be locally hosted only
+    let newItem = `<div class="forum-item">
+                    <div class="forum-image">
+                        <img src="${imageFile}">
+                    </div>
+                    <div class="forum-labels">
+                        <label><b>Item</b>: ${itemText.value}</label><br>
+                        <label><b>Last Seen</b>: ${foundLocation.value}</label><br>
+                        <label><b>Lost On</b>: ${date}</label><br>
+                        <label><b>Email Me</b>: tester@wpi.edu</label>
+                    </div>
+                </div>`;
+
+    let foundContainer = document.getElementById("lost-header");
+    foundContainer.insertAdjacentHTML("afterend", newItem);
+
+    // Reset fields and close the form
+    itemText.value = '';
+    foundLocation.value = '';
+    image.value = '';
+    closeForm("found-form");
+};
 
 function resetAllForms() {
     // Get all fields from found item form
@@ -68,9 +106,15 @@ function resetAllForms() {
     let image = document.getElementById("found-img");
 
     // Get all fields from lost item form
+    let lostItemText = document.getElementById("item-desc-lost");
+    let lastSeen = document.getElementById("last-seen");
+    let lostImage = document.getElementById("lost-img");
 
     // Reset fields
     itemText.value = '';
     foundLocation.value = '';
     image.value = '';
-}
+    lostItemText.value = '';
+    lastSeen.value = '';
+    lostImage.value = '';
+};
